03ac8cce78d3157383acb880cdd4d85e3a788400
ding <dingwuying@tutanota.com>
2022-06-11 19:22:46 +0300

    Nav bar

--- imports/ui/components/Navbar/Navbar.jsx
+++ imports/ui/components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import { Menu } from 'antd';
+import { Menu, Col, Row } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
+import './NavbarStyle.css';
 
 export const Navbar = () => {
-  const items = [
+  const leftItems = [
     { label: 'Home', key: 'home' },
     { label: 'About', key: 'about' },
     { label: 'Contact', key: 'contact' },
-    {
-      label: 'connect',
-      key: 'connect',
-      children: [
-        { label: 'Sign up', key: 'Signup' },
-        { label: 'Sign in', key: 'Signin' },
-      ],
-    },
   ];
-  return <Menu items={items} mode="horizontal" />;
+
+  const rightItems = [
+    { label: 'Sign up', key: 'Signup' },
+    { label: 'Sign in', key: 'Signin' },
+    { key: 'giticon', icon: <GithubOutlined /> },
+  ];
+
+  return (
+
+    <Row>
+      <Col flex={9}>
+        <Menu items={leftItems} mode="horizontal" className="navbar" />
+      </Col>
+      <Col flex={1}>
+        <Menu items={rightItems} mode="horizontal" className="navbar" />
+      </Col>
+    </Row>
+  );
 };
